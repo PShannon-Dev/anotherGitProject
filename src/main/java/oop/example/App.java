7036b94bae3a2f9e0880ec009b16cc250de4565b
paulshannon-coding <mikey4295@hotmail.com>
2021-05-22 18:48:21 -0400

    These are my changes

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -2,14 +2,12 @@ package oop.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
 
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Person person = new Person();
+        System.out.println(person);
     }
-    var person = new Person ();
 }
